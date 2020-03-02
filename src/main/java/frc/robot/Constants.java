869cabcf269ef6dada604136b4a3b77d1ed28d5d
Robotics <dakotaelle2@gmail.com>
2020-03-02 15:59:14 -0800

    One more

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -92,7 +92,7 @@ public interface Constants {
     // 1 encouder rotation: 4096 counts = (230/16) * 9
     // 240 counts --> 1 turret rotation
     // 1 degree = 240 counts/360 degrees
-    final static double one_turret_degree = (((4096.0 * (1.0/9.0) * 230.0/16) / 360)) * (5.0/3.0);
+    final static double one_turret_degree = (((4096.0 * 9.0 * 230.0/16) / 360));
     
 
     
